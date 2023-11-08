ed7c4f40154d24f381d72457c283351dd0d3d928
Miguel angel gonzalo lacava <gonzalolacava@hotmail.com>
2023-11-08 04:35:47 -0300

    1.6

--- django1/settings.py
+++ django1/settings.py
@@ -99,7 +99,6 @@ DATABASES = {
 #     }
 # }
 
-hola = "holas"
 
 
 
@@ -145,5 +144,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
