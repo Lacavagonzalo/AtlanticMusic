ab56bb30a95a1310d24bc2e5ef06f38f0304788a
Miguel angel gonzalo lacava <134579791+Lacavagonzalo@users.noreply.github.com>
2023-11-08 02:33:43 -0300

    Add files via upload

---
+++ build_file.sh
@@ -0,0 +1,2 @@
+pip install -r requeriments.txt
+python3.9 manage.py collectstatic
---
+++ django1/urls.py
@@ -0,0 +1,16 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+urlpatterns = [
+    path('', include('myapp.urls')),
+    path('item/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
